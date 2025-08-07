1022f9d83863b11f587718c517c99f93ad86beeb
devmissionaction <dev.missionaction@gmail.com>
2025-08-07 17:26:38 +0200

    add schemas index

--- schemaTypes/index.ts
+++ schemaTypes/index.ts
@@ -2,5 +2,11 @@ import blockContent from './blockContent'
 import category from './category'
 import post from './post'
 import author from './author'
+import homePage from './homePage'
+import issue from './issue'
+import aboutPage from './aboutPage'
+import teamMember from './teamMember'
+import contactPage from './contactPage'
+import numeroPage from './numeroPage'
 
-export const schemaTypes = [post, author, category, blockContent]
+export const schemaTypes = [post, author, category, blockContent, homePage, issue, aboutPage, teamMember, contactPage, numeroPage]
