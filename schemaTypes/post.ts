6ca8151db863af761ead23435eceb093858d7499
Lucieguerin431 <lucie.guerin@edu.univ-eiffel.fr>
2026-03-09 22:46:27 +0100

    Add pdf to post

--- schemaTypes/post.ts
+++ schemaTypes/post.ts
@@ -7,72 +7,38 @@ export default defineType({
   fields: [
     defineField({
       name: 'title',
-      title: 'Title',
+      title: 'Titre',
       type: 'string',
-    }),
-    defineField({
-      name: 'slug',
-      title: 'Slug',
-      type: 'slug',
-      options: {
-        source: 'title',
-        maxLength: 96,
-      },
-    }),
-    defineField({
-      name: 'author',
-      title: 'Author',
-      type: 'reference',
-      to: {type: 'author'},
-    }),
-    defineField({
-      name: 'mainImage',
-      title: 'Main image',
-      type: 'image',
-      options: {
-        hotspot: true,
-      },
-    }),
-    defineField({
-      name: 'categories',
-      title: 'Categories',
-      type: 'array',
-      of: [{type: 'reference', to: {type: 'category'}}],
-    }),
-    defineField({
-      name: 'publishedAt',
-      title: 'Published at',
-      type: 'datetime',
-    }),
-    defineField({
-      name: 'body',
-      title: 'Body',
-      type: 'blockContent',
+      validation: (Rule) => Rule.required(),
     }),
     defineField({
       name: 'type',
-      title: 'Type d’article',
+      title: "Type d'article",
       type: 'string',
       options: {
         list: [
-          { title: 'Gratuit', value: 'free' },
-          { title: 'Payant', value: 'paid' },
+          {title: 'Gratuit', value: 'free'},
+          {title: 'Payant', value: 'paid'},
         ],
         layout: 'radio',
       },
-      validation: Rule => Rule.required(),
+      validation: (Rule) => Rule.required(),
+    }),
+    defineField({
+      name: 'pdf',
+      title: 'PDF document',
+      type: 'file',
+      options: {
+        accept: 'application/pdf',
+      },
+      validation: (Rule) => Rule.required(),
     }),
   ],
 
   preview: {
     select: {
       title: 'title',
-      author: 'author.name',
-      media: 'mainImage',
-    },
-    prepare(selection) {
-      const {author} = selection
-      return {...selection, subtitle: author && `by ${author}`}
+      subtitle: 'type',
     },
   },
 })
